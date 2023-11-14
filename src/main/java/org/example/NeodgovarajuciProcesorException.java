f0bce00b44ac3dc72d2ee8a4fbf0f81796e6ad50
Zehra Buza <zbuza1@etf.unsa.ba>
2023-11-14 23:29:08 +0100

    New branch

---
+++ src/main/java/org/example/NeodgovarajuciProcesorException.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class NeodgovarajuciProcesorException extends RuntimeException{
+    public NeodgovarajuciProcesorException(String poruka){
+        super(poruka);
+    }
+}
