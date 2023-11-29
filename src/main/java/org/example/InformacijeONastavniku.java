ef012321a40899b891e7178a24c448f4cac46912
Zehra Buza <zbuza1@etf.unsa.ba>
2023-11-29 20:00:19 +0100

    New branch

---
+++ src/main/java/org/example/InformacijeONastavniku.java
@@ -0,0 +1,26 @@
+package org.example;
+import java.util.ArrayList;
+
+public class InformacijeONastavniku extends LicneInformacije implements InterfejsPredstavi {
+    private String titula;
+    private ArrayList<Ocjena> ocjene;
+
+    public String getTitula() {
+        return titula;
+    }
+
+    public void setTitula(String titula) {
+        this.titula = titula;
+    }
+
+    public ArrayList<Ocjena> getOcjene() { return ocjene; }
+
+    public void setOcjene(ArrayList<Ocjena> ocjene) {
+        this.ocjene = ocjene;
+    }
+
+    @Override
+    public String predstavi() {
+        return getTitula() + " " + getIme() + " " + getPrezime();
+    }
+}
---
+++ src/main/java/org/example/Predmet.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Predmet implements InterfejsPredstavi {
+    private String naziv, opis;
+    private ArrayList<Ocjena> ocjene;
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    public ArrayList<Ocjena> getOcjene() { return ocjene; }
+
+    public void setOcjene(ArrayList<Ocjena> ocjene) {
+        this.ocjene = ocjene;
+    }
+
+    @Override
+    public String predstavi() {
+        return "Naziv: " + naziv + " opis: " + opis;
+    }
+}
